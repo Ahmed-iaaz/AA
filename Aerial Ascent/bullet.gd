bf3b37e0abb6063937830744e6661755cdf8fa5c
Ahmed-iaaz <121626250+Ahmed-iaaz@users.noreply.github.com>
2023-10-20 10:55:37 +0500

    Update bullet.gd

--- Aerial Ascent/bullet.gd
+++ Aerial Ascent/bullet.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	move_and_slide()
+	
+	if velocity == Vector2.ZERO:
+		queue_free()
 
 func _on_area_2d_body_entered(body):
 	if body.name == "player":
